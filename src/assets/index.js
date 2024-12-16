df7bf0c5281ee7f1fa19fbcf1bde5c66820f7513
unknown <mittalyas1234@gmail.com>
2024-12-16 10:55:23 +0530

    portfolio done

---
+++ src/assets/index.js
@@ -0,0 +1,73 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import web from "./web.png";
+import github from "./github.png";
+import live_version from './live_version.png'
+
+import close from './close.svg'
+import menu from './menu.svg'
+import mobile from './mobile.png'
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import python from './tech/python.png'
+import Express from './tech/express.png'
+import Sass from './tech/Sass.png'
+import Postman from './tech/Postman.png'
+import Clang from './tech/C++.png'
+import jwt from './tech/jwt.png'
+
+import DulitFest from './company/DulitFest.png';
+import GeekClash from "./company/GeekClash.png";
+import bizoally from './company/bizoally.webp';
+import Sportzpod from './company/Sportzpod.png'
+
+import contactlottie from './Contact.json'
+import geekClash from './geekClash.png'
+import Jscop from './Jscop.png'
+import coadeaz from './coadeaz.png'
+import secure from './secure.png'
+import sports from './sports.png'
+import dulitfestphoto from './workdulitfest.png'
+
+
+
+export {
+  logo,
+  web,
+  github,
+  live_version,
+  css,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  python,
+  Sportzpod,
+Express,
+  contactlottie,
+  bizoally,
+  GeekClash,
+  Sass,
+Clang,
+Postman,
+geekClash,
+coadeaz,
+Jscop,
+secure,
+sports,
+DulitFest,
+dulitfestphoto,
+jwt,backend,close,menu,
+mobile
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon, description }) => (
+  <Tilt className='xs:w-[250px] w-full '>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-8 px-8 flex flex-col justify-between items-center h-[320px] sm:py-3' // Set a fixed height
+      >
+        <img
+          src={icon}
+          alt={title.toLowerCase()}
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+
+        <p className='text-white text-[14px] text-left mt-0'>
+          {description}
+        </p>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] leading-[30px]'
+      >
+        I am a MERN stack developer with a passion for building dynamic and engaging web applications. Leveraging my expertise in both front-end and back-end technologies, I create responsive and user-friendly interfaces that enhance digital experiences. By merging creativity with technical skills, I aim to deliver innovative solutions that meet the needs of users and clients alike. From initial concept to final deployment, I am committed to exceeding expectations and ensuring seamless functionality in the fast-paced world of web development.
+      </motion.p>
+
+      <div
+  className="mt-10 flex flex-wrap gap-10  lg:justify-between md:justify-center justify-center"
+>
+  {services.map((service, index) => (
+    <ServiceCard key={service.title} index={index} {...service} />
+  ))}
+</div>
+
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
